efaa16016a45b2137be71e361f16ad0d79e91cbb
Nicholas <nickmx1896@Hotmail.com>
2018-08-31 21:29:46 +0800

    changed export file

--- export.sh
+++ export.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 export ARCH=arm64
 export SUBARCH=arm64
-export CROSS_COMPILE=hello
+export CROSS_COMPILE=/home/nicholas/build/toolchain/aarch64-linux-android-4.9/bin/aarch64-linux-android-
